8642f5ccf377bf5d32c5f4cf949763a7f1c2e5bb
Andrew Le <andrewle19@csu.fullerton.edu>
2018-03-16 23:07:32 -0700

    Added extract file path/ dynamic content type and display html

---
+++ index.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+// var path = require('path'); replace with tthe extract function
+var extract = require('./extract');
+const mime = require('mime');
+
+
+var server = http.createServer(function (req, res){
+
+
+  console.log('Responding to a request');
+
+  // replace with extract
+  // res.end('<h1>Testing</h1>');
+  // var url = req.url;
+  // var fileName = 'index.html';
+  // if(url.length > 1){
+  //   fileName = url.substring(1);
+  // }
+  // var filePath = path.resolve(__dirname, 'app', fileName);
+  //console.log(fileName);
+
+  var handleError = function (err, res){
+    res.writeHead(404);
+    fs.readFile('app/error.html', function(err,data){
+      res.end(data);
+    });
+  }
+
+  // extract the path to file
+  var filePath = extract(req.url);
+
+  fs.readFile(filePath, function(err, data){
+
+    if(err){
+      handleError(err, res);
+      return;
+    } else {
+      console.log(mime.getType(filePath));
+      res.setHeader('Content-Type',mime.getType(filePath));
+      res.end(data);
+    }
+
+  });
+});
+
+server.listen(3000);
